04e0c4bdf45c59ade46dd44cda4a3aafd2e2a400
HARIHARAN V S <iamhariharan.offcl@gmail.com>
2025-12-15 15:47:57 +0530

    Made change on page reloading error

--- Chat-Application/src/main.jsx
+++ Chat-Application/src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
